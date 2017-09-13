374d94b32d05e43700d9d5637e02b61c5a853c98
ahmadeeva-su <ahmadeeva.su@gmail.com>
2017-09-13 11:15:46 +0500

    Updated default version of FB api; updated gem version

--- lib/facepalm/rails/helpers/javascript_helper.rb
+++ lib/facepalm/rails/helpers/javascript_helper.rb
@@ -29,7 +29,7 @@ module Facepalm
             :xfbml        => true,
             :frictionless => true,
             :locale       => "en_US",
-            :version      => "v2.2"
+            :version      => "v2.9"
           )
 
           extra_js = capture(&block) if block_given?
